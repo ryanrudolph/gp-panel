104ddd09ebff259a27f36dc8d045f0f36261eb9e
Ryan Rudolph <rudolphdesigns@gmail.com>
2018-01-15 16:31:33 -0500

    Update class-gp-panel-admin.php

--- admin/class-gp-panel-admin.php
+++ admin/class-gp-panel-admin.php
@@ -147,7 +147,7 @@ function gp_welcome_panel() {
 		echo '<h3>Welcome to <strong>' . esc_attr( get_bloginfo( 'title' ) ) . '</strong> Content Management System</h3>';
 		echo 'Designed and Developed by getphound.com';
 		echo '<a href="https://getphound.com/" target="_blank" class="gp-logo"><img src="https://getphound.com/wp-content/themes/getphound/images/logo.png" alt="GetPhound" /></a>';
-		echo 'For questions or technical support call (610) 999-1808 or email <a href="mailto:josh@getphound.com">josh@getphound.com</a>.';
+		echo 'For questions or technical support call (610) 897 8127 or email <a href="mailto:josh@getphound.com">josh@getphound.com</a>.';
 	echo '</div>';
 
 }
@@ -180,25 +180,17 @@ add_action('wp_dashboard_setup', 'gp_remove_dashboard_widgets' );
 /* --------------------------------------------------------------------
 Remove Unwanted Menu Items from WordPress Admin
 -------------------------------------------------------------------- */
-function gp_remove_admin_menus (){
- 
-	// Check that the built-in WordPress function remove_menu_page() exists in the current installation
-	if ( function_exists('remove_menu_page') ) {
-	 
-		/* Remove unwanted menu items by passing their slug to the remove_menu_item() function.
-		You can comment out the items you want to keep. */
-		 
-		remove_menu_page('link-manager.php'); // Links
-		remove_menu_page('edit-comments.php'); // Comments
-		remove_menu_page('plugins.php'); // Plugins
-		remove_submenu_page( 'themes.php', 'customize.php' );
-		remove_submenu_page( 'themes.php', 'themes.php' );
- 		remove_submenu_page('themes.php', 'theme-editor.php');
-		remove_menu_page('tools.php'); // Tools
-		remove_menu_page('options-general.php'); // Settings
-	 
-	}
- 
+function gp_remove_admin_menus (){ 
+	/* Remove unwanted menu items by passing their slug to the remove_menu_item() function.
+	You can comment out the items you want to keep. */
+	remove_menu_page('link-manager.php'); // Links
+	remove_menu_page('edit-comments.php'); // Comments
+	remove_menu_page('plugins.php'); // Plugins
+	remove_submenu_page( 'themes.php', 'customize.php' );
+	remove_submenu_page( 'themes.php', 'themes.php' );
+	remove_submenu_page( 'themes.php', 'theme-editor.php');
+	remove_menu_page('tools.php'); // Tools
+	remove_menu_page('options-general.php'); // Settings
 }
 
 // Add our function to the admin_menu action
